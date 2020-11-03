52cfcd85096c2ebaa8825f8fcdeed09a4bea607e
Nicholas <nlamonaco86@gmail.com>
2020-11-03 13:57:34 -0500

    separated order/user api routes

--- routes/order-api-routes.js
+++ routes/order-api-routes.js
@@ -1,55 +1,9 @@
 // Require models and passport
 require('dotenv').config();
 const db = require("../models");
-const passport = require("../config/passport");
 const order = require("../config/order.js");
 
 module.exports = function (app) {
-  // LOGIN route with error handling
-  app.post("/api/login", passport.authenticate("local"), function (req, res) {
-    res.json(req.user);
-  });
-
-  // SIGNUP route with error handling
-  app.post("/api/signup", function (req, res) {
-    db.User.create({
-      email: req.body.email,
-      password: req.body.password,
-      first: req.body.first,
-      last: req.body.last,
-      position: req.body.position,
-      phone: req.body.phone,
-      dob: req.body.dob,
-      ssn: req.body.ssn,
-    })
-      .then(function () {
-        res.redirect(307, "/api/login");
-      })
-      .catch(function (err) {
-        res.status(401).json(err);
-      });
-  });
-
-  // LOGOUT route
-  app.get("/logout", function (req, res) {
-    req.logout();
-    res.redirect("/");
-  });
-
-  // user data client-side route
-  app.get("/api/user_data", function (req, res) {
-    (!req.user ? res.json({}) : res.json({
-      email: req.user.email,
-      phone: req.user.phone,
-      id: req.user.id,
-      first: req.user.first,
-      last: req.user.last,
-      position: req.user.position,
-      cloudUploadName: process.env.CLOUDINARY_CLOUDNAME,
-      cloudUploadPreset: process.env.CLOUDINARY_UPLOAD_PRESET
-    }))
-  });
-
   app.post("/api/orders", function (req, res) {
     // Data from the form gets inserted into three separate but associated tables
     // Customer/Vehicle ID's arrive from client-side to avoid any promise/async issues
